86242f756fe0fe25dc475f8c34cdeb5122292e0d
iyashraj <yashr3037@gmail.com>
2024-03-13 10:35:19 +0530

    setting schema added

--- server/src/models/usercontact.models.js
+++ server/src/models/usercontact.models.js
@@ -1,32 +1,33 @@
-import mongoose , {Schema} from "mongoose";
+import mongoose, {Schema} from "mongoose";
 
-const userContactSchema = new Schema(
-    {
-      user_id: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-      },
-      contact_id: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-      },
-      added_at: {
-        type: Date,
-        default: Date.now
-      },
-      lastInteraction: {
-        type: Date,
-        default: Date.now
-      },
-      status: {
-        type: String,
-        default: "not connected"
-      }
+
+const contactSchema = new Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
     },
-    { timestamps: true }
-  );
+    contact_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    added_at: {
+      type: Date,
+      default: Date.now
+    },
+    lastInteractionAt: {
+      type: Date,
+      default: Date.now
+    },
+    status: {
+      type: String,
+      default: "not connected"
+    }
+  },
+  { timestamps: true }
+);
 
 // Indexes
-userContactSchema.index({ user_id: 1, contact_id: 1 });
+contactSchema.index({ user_id: 1, contact_id: 1 });
 
-export const UserContact = mongoose.model("userContact", userContactSchema)
+export const UserContact = mongoose.model("UserContact", contactSchema)
