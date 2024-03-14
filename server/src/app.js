16e8d949f1835a3cd5f33abca40a1e0dd93dd94a
iyashraj <yashr3037@gmail.com>
2024-03-14 06:44:40 +0530

    initial routes, middleware and index setup

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,12 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
+app.use(morgan("dev"));
+app.use(express.json({limit: "16kb"}))    // for reading JSON
+app.use(express.urlencoded({extended: true, limit: "16kb"}))    // for reading query data from URL
+app.use(express.static("public"))   // for storing any images/document/etcs.
+app.use(cookieParser()) // for read & write cookies
+export { app };
