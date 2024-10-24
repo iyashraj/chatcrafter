54bd04b1796c44a92923bf89ec1e24e095bcbeac
Yash Raj <yashr3037@gmail.com>
2024-10-24 12:07:11 +0530

    changing entire backend

--- server/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { loginUserHandler, registerUserHandler } from '../controllers/auth.controllers.js';
-
-const Router = express.Router();
-Router.post("/", registerUserHandler);
-Router.post("/login", loginUserHandler);
-
-export default Router;
